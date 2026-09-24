c18fac77a13eae09ef1ad481735be687fbf9d03e
JpSotoSanchez <0262205@up.edu.mx>
2026-09-24 17:58:46 -0600

    started players 2 and done, maybe 2

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -51,7 +51,7 @@ public class playerMovement : MonoBehaviour
         switch (i)
         {
             case(1):
-                trans.rotation = new Quaternion(trans.rotation.x+90, trans.rotation.y, trans.rotation.z, 1);
+                trans.rotation = new Quaternion(trans.rotation.x, trans.rotation.y, trans.rotation.z+2, 1);
             break;
             
             case(2):
